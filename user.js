0bace2a0263512cd74bfa04fb583e34c9a9f7c76
NatchaRunsri <natcha.runs@ku.th>
2025-03-13 02:58:52 +0700

    retouch

--- user.js
+++ user.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'http://localhost:8000'
 
-window.onload =  async () => {
+window.onload = async () => {
     await loadData()
 }
-const loadData =async () => {
+const loadData = async () => {
     console.log('User page loaded')
     //1. load user ทั้งหมด จาก API ที่เตรียมไว้
     const response = await axios.get(`${BASE_URL}/users`)
@@ -13,12 +13,25 @@ const loadData =async () => {
     const userDom = document.getElementById('user')
     //2. นำ user ทั้งหมด โหลดกลับเข้าไปใน html (คือเอาไปแสดงใน html)
     let htmlData = `<div>`
-    for (let i=0; i<response.data.length; i++) {
+    for (let i = 0; i < response.data.length; i++) {
         let user = response.data[i]
         htmlData += `<div>
-        ${user.id} ${user.firstname} ${user.lastname}
-        <a href='index_1.html?id=${user.id}'><button>Edit</button></a>
-        <button class='delete' data-id='${user.id}'>Delete</button>
+        <table>
+         <tr class='header-form'>
+            <th>ID</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+            <tr class='data-form'> 
+                <td class='row1'>${user.id}</td>
+                <td class='row2'>${user.firstname}</td>
+                <td>${user.lastname}</td>
+                <td> <a href='index_1.html?id=${user.id}'><button class='edit'>Edit</button></a></td>
+                <td><button class='delete' data-id='${user.id}'>Delete</button></td>
+            </tr>          
+        </table>
         </div>`
     }
     htmlData += `</div>`
@@ -26,14 +39,14 @@ const loadData =async () => {
 
     //3. ลบ user
     const deleteDoms = document.getElementsByClassName('delete')
-    for (let i=0; i<deleteDoms.length; i++) {
-        deleteDoms[i].addEventListener('click', async (event) =>{
+    for (let i = 0; i < deleteDoms.length; i++) {
+        deleteDoms[i].addEventListener('click', async (event) => {
             //ดึง id ของ user ที่ต้องการลบ
             const id = event.target.dataset.id
-            try{
+            try {
                 await axios.delete(`${BASE_URL}/users/${id}`)
                 loadData() //recursive function =   เรียกใช้ฟังกก์ชันตัวเอง
-            }catch(error) {
+            } catch (error) {
                 console.error('error', error)
             }
         })
